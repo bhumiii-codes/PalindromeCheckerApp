ab4dca0a030f3344bace11f22caa75c6ea4caf00
bhumiii-codes <bhumikaaa31@gmail.com>
2026-02-23 15:24:30 +0530

    UC3: Add user input palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -6,7 +8,11 @@ public class PalindromeCheckerApp {
         System.out.println(" Welcome to Palindrome Checker ");
         System.out.println("=================================");
 
-        String word = "madam";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+
         String reversed = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
@@ -19,5 +25,6 @@ public class PalindromeCheckerApp {
             System.out.println(word + " is NOT a Palindrome");
         }
 
+        scanner.close();
     }
 }
