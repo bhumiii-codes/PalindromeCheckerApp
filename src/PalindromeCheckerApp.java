a361f4a56ca64e8770c19e1cb727ad52d211c723
bhumiii-codes <bhumikaaa31@gmail.com>
2026-02-23 15:00:05 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+
+    }
+}
