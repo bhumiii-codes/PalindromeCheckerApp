f85656492f4e89c448c7920d9afddc95761c97d5
bhumiii-codes <bhumikaaa31@gmail.com>
2026-02-23 15:33:29 +0530

    UC7: Refactor to OOP structure

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,34 +4,26 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        PalindromeCheckerApp app = new PalindromeCheckerApp();
+        app.start();
+    }
+
+    public void start() {
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
 
-            System.out.println("\n=================================");
-            System.out.println("      PALINDROME CHECKER APP     ");
-            System.out.println("=================================");
-            System.out.println("1. Check Palindrome");
-            System.out.println("2. Exit");
-            System.out.print("Enter your choice: ");
-
+            displayMenu();
             int choice = scanner.nextInt();
-            scanner.nextLine(); // consume newline
+            scanner.nextLine();
 
             if (choice == 1) {
 
                 System.out.print("Enter text: ");
                 String word = scanner.nextLine();
 
-                word = word.replaceAll("\\s+", "");
-
-                String reversed = "";
-
-                for (int i = word.length() - 1; i >= 0; i--) {
-                    reversed += word.charAt(i);
-                }
-
-                if (word.equalsIgnoreCase(reversed)) {
+                if (isPalindrome(word)) {
                     System.out.println("Palindrome ✅");
                 } else {
                     System.out.println("Not a Palindrome ❌");
@@ -43,12 +35,32 @@ public class PalindromeCheckerApp {
                 break;
 
             } else {
-
-                System.out.println("Invalid choice. Try again.");
-
+                System.out.println("Invalid choice.");
             }
         }
 
         scanner.close();
+    }
+
+    public void displayMenu() {
+        System.out.println("\n=================================");
+        System.out.println("      PALINDROME CHECKER APP     ");
+        System.out.println("=================================");
+        System.out.println("1. Check Palindrome");
+        System.out.println("2. Exit");
+        System.out.print("Enter your choice: ");
+    }
+
+    public boolean isPalindrome(String word) {
+
+        word = word.replaceAll("\\s+", "");
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        return word.equalsIgnoreCase(reversed);
     }
 }
