26a083fe97c50a5691290516f8b17db961f0aea7
bhumiii-codes <bhumikaaa31@gmail.com>
2026-03-02 13:55:06 +0530

    UC10: Ignore spaces and case using string normalization

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,9 +7,11 @@ public class PalindromeCheckerApp {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter text: ");
-        String input = scanner.nextLine().replaceAll("\\s+", "").toLowerCase();
+        String input = scanner.nextLine();
 
-        if (isPalindrome(input, 0, input.length() - 1)) {
+        String normalized = normalize(input);
+
+        if (isPalindrome(normalized)) {
             System.out.println("Palindrome ✅");
         } else {
             System.out.println("Not a Palindrome ❌");
@@ -18,20 +20,30 @@ public class PalindromeCheckerApp {
         scanner.close();
     }
 
-    // Recursive method
-    public static boolean isPalindrome(String str, int start, int end) {
+    // Step 1: Normalize string
+    public static String normalize(String input) {
 
-        // Base Condition
-        if (start >= end) {
-            return true;
+        return input
+                .replaceAll("\\s+", "")   // remove spaces
+                .toLowerCase();           // convert to lowercase
+    }
+
+    // Step 2: Apply palindrome logic
+    public static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
         }
 
-        // If mismatch
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
+        return true;
     }
 }
